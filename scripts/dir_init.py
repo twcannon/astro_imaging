d025380efb5c17ac623b4272caa5621e386ba77f
Thomas <tcannon@thomas-lubuntu.(none)>
2017-05-18 00:24:26 -0400

    started dir_init.py script

--- scripts/dir_init.py
+++ scripts/dir_init.py
@@ -1,2 +1,9 @@
 #!/usr/bin/python
-print 'hello'
+import time,re
+
+image_content = ""
+while not (len(image_content)>0 and re.match("^(?!_)\w*(?<!_)$",image_content)):
+	image_content = raw_input('\nWhat are you imaging? (Ex. jupiter, moon, m31...)\n'+
+                'Please use only alphanumeric characters (a-z,0-9)\n')
+
+dir_name = (time.strftime("%Y%m%d"))+'_'+image_content
