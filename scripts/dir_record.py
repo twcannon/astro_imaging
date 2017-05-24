aa4684f4981f4cdecb9d6549a40b05f2c9ee2327
Thomas Cannon <tcannon.mail@gmail.com>
2017-05-23 22:50:17 -0400

    small changes to dir_record modified:   dir_record.py

--- scripts/dir_record.py
+++ scripts/dir_record.py
@@ -1,2 +1,8 @@
 #!/usr/bin/python
-print 'hello'
+
+from astropy.io import fits
+
+image = fits.open('../data/20170517_saturn/190ms/red/Saturnp19r_001.fit')
+
+for name in image[0].header:
+	print name + " = " + str(image[0].header[name])
