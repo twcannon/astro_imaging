217a399bf7d0d0ee66b74a494ecc07a5a871b776
Stephen Lesage <lesagesj@Stephens-MacBook-Pro.local>
2017-05-27 18:17:05 -0400

    added all of the image files and began to do image processing for Jupiter_Red_Images

---
+++ analysis/Observing_5_17_2017/readme.txt
@@ -0,0 +1,3 @@
+All images taken with additional IR blocking filter
+For Saturn images, exposure time is .019 s (can check headers also)
+For venus G images, typo in files names RG
---
+++ analysis/RGB_image_analysis.py
@@ -0,0 +1,26 @@
+import pyfits
+import matplotlib
+import numpy
+#import pyplot
+
+
+jupiter_blue_001 = pyfits.open('/Users/lesagesj/PycharmProjects/astro_imaging/analysis/Observing_5_10_2017/Jupiter/p25Seconds/JupiterB_001.fit')
+jupiter_blue_002 = pyfits.open('/Users/lesagesj/PycharmProjects/astro_imaging/analysis/Observing_5_10_2017/Jupiter/p25Seconds/JupiterB_002.fit')
+jupiter_blue_003 = pyfits.open('/Users/lesagesj/PycharmProjects/astro_imaging/analysis/Observing_5_10_2017/Jupiter/p25Seconds/JupiterB_003.fit')
+jupiter_blue_004 = pyfits.open('/Users/lesagesj/PycharmProjects/astro_imaging/analysis/Observing_5_10_2017/Jupiter/p25Seconds/JupiterB_004.fit')
+jupiter_blue_005 = pyfits.open('/Users/lesagesj/PycharmProjects/astro_imaging/analysis/Observing_5_10_2017/Jupiter/p25Seconds/JupiterB_005.fit')
+jupiter_blue_006 = pyfits.open('/Users/lesagesj/PycharmProjects/astro_imaging/analysis/Observing_5_10_2017/Jupiter/p25Seconds/JupiterB_006.fit')
+jupiter_blue_007 = pyfits.open('/Users/lesagesj/PycharmProjects/astro_imaging/analysis/Observing_5_10_2017/Jupiter/p25Seconds/JupiterB_007.fit')
+jupiter_blue_008 = pyfits.open('/Users/lesagesj/PycharmProjects/astro_imaging/analysis/Observing_5_10_2017/Jupiter/p25Seconds/JupiterB_008.fit')
+jupiter_blue_009 = pyfits.open('/Users/lesagesj/PycharmProjects/astro_imaging/analysis/Observing_5_10_2017/Jupiter/p25Seconds/JupiterB_009.fit')
+jupiter_blue_010 = pyfits.open('/Users/lesagesj/PycharmProjects/astro_imaging/analysis/Observing_5_10_2017/Jupiter/p25Seconds/JupiterB_010.fit')
+jupiter_blue_011 = pyfits.open('/Users/lesagesj/PycharmProjects/astro_imaging/analysis/Observing_5_10_2017/Jupiter/p25Seconds/JupiterB_011.fit')
+jupiter_blue_012 = pyfits.open('/Users/lesagesj/PycharmProjects/astro_imaging/analysis/Observing_5_10_2017/Jupiter/p25Seconds/JupiterB_012.fit')
+jupiter_blue_013 = pyfits.open('/Users/lesagesj/PycharmProjects/astro_imaging/analysis/Observing_5_10_2017/Jupiter/p25Seconds/JupiterB_013.fit')
+jupiter_blue_014 = pyfits.open('/Users/lesagesj/PycharmProjects/astro_imaging/analysis/Observing_5_10_2017/Jupiter/p25Seconds/JupiterB_014.fit')
+jupiter_blue_015 = pyfits.open('/Users/lesagesj/PycharmProjects/astro_imaging/analysis/Observing_5_10_2017/Jupiter/p25Seconds/JupiterB_015.fit')
+
+#print(jupiter_blue_001)
+#print(len(jupiter_blue_001))
+#print(jupiter_blue_001.info())
+######jupiter_blue_001.show()
