3c0d97066b8f7da2a18a5c7193b5ebd79e6f87c2
Stephen Lesage <lesagesj@Stephens-MacBook-Pro.local>
2017-05-29 17:35:53 -0400

    initial combination of images, not yet alligned

--- analysis/RGB_image_analysis.py
+++ analysis/RGB_image_analysis.py
@@ -1,7 +1,8 @@
 from astropy.io import fits
 import matplotlib.pyplot as plt
+import numpy as np
 
-
+#import fits files as hdulist
 jupiter_blue_001 = fits.open('Observing_5_10_2017/Jupiter/p25Seconds/JupiterB_001.fit')
 jupiter_blue_002 = fits.open('Observing_5_10_2017/Jupiter/p25Seconds/JupiterB_002.fit')
 jupiter_blue_003 = fits.open('Observing_5_10_2017/Jupiter/p25Seconds/JupiterB_003.fit')
@@ -18,13 +19,50 @@ jupiter_blue_013 = fits.open('Observing_5_10_2017/Jupiter/p25Seconds/JupiterB_01
 jupiter_blue_014 = fits.open('Observing_5_10_2017/Jupiter/p25Seconds/JupiterB_014.fit')
 jupiter_blue_015 = fits.open('Observing_5_10_2017/Jupiter/p25Seconds/JupiterB_015.fit')
 
-jupiter_blue_001_image = jupiter_blue_001[0].data
-plt.imshow(jupiter_blue_001_image, cmap='Blues')
-plt.colorbar()
-plt.show()
 
-#jupiter_moons_blue_001 = fits.open('/Users/lesagesj/PycharmProjects/astro_imaging/analysis/Observing_5_10_2017/Jupiter/p1Second/JupiterB_001.fit')
-#jupiter_moons_blue_001_image = jupiter_moons_blue_001[0].data
-#plt.imshow(jupiter_moons_blue_001_image, cmap='Blues')
+#converting hdulist into numpy arrays
+jupiter_blue_001_image = jupiter_blue_001[0].data
+jupiter_blue_002_image = jupiter_blue_002[0].data
+jupiter_blue_003_image = jupiter_blue_003[0].data
+jupiter_blue_004_image = jupiter_blue_004[0].data
+jupiter_blue_005_image = jupiter_blue_005[0].data
+jupiter_blue_006_image = jupiter_blue_006[0].data
+jupiter_blue_007_image = jupiter_blue_007[0].data
+jupiter_blue_008_image = jupiter_blue_008[0].data
+jupiter_blue_009_image = jupiter_blue_009[0].data
+jupiter_blue_010_image = jupiter_blue_010[0].data
+jupiter_blue_011_image = jupiter_blue_011[0].data
+jupiter_blue_012_image = jupiter_blue_012[0].data
+jupiter_blue_013_image = jupiter_blue_013[0].data
+jupiter_blue_014_image = jupiter_blue_014[0].data
+jupiter_blue_015_image = jupiter_blue_015[0].data
+
+#test to view the numpy array as an image
+#plt.imshow(jupiter_blue_001_image, cmap='Blues')
 #plt.colorbar()
 #plt.show()
+
+image_stack_array = []
+image_stack_array.append(jupiter_blue_001_image)
+image_stack_array.append(jupiter_blue_002_image)
+image_stack_array.append(jupiter_blue_003_image)
+image_stack_array.append(jupiter_blue_004_image)
+image_stack_array.append(jupiter_blue_005_image)
+image_stack_array.append(jupiter_blue_006_image)
+image_stack_array.append(jupiter_blue_007_image)
+image_stack_array.append(jupiter_blue_008_image)
+image_stack_array.append(jupiter_blue_009_image)
+image_stack_array.append(jupiter_blue_010_image)
+image_stack_array.append(jupiter_blue_011_image)
+image_stack_array.append(jupiter_blue_012_image)
+image_stack_array.append(jupiter_blue_013_image)
+image_stack_array.append(jupiter_blue_014_image)
+image_stack_array.append(jupiter_blue_015_image)
+
+
+final_image = np.median(image_stack_array, axis=0)
+#final_image = np.zeros(shape=image_stack_array[0].shape)
+
+plt.imshow(final_image, cmap='gray')
+plt.colorbar()
+plt.show()
