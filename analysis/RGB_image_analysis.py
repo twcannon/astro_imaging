a47f6d477527b8a36a0b342ca2a7447e9bebad15
lesagesj <lesagesj@gmail.com>
2017-05-30 20:55:39 -0400

    found dimensions of image and added someone else's code that needs to be heavily edited for our use

--- analysis/RGB_image_analysis.py
+++ analysis/RGB_image_analysis.py
@@ -1,6 +1,7 @@
 from astropy.io import fits
 import matplotlib.pyplot as plt
 import numpy as np
+#import cv2
 
 #import fits files as hdulist
 jupiter_blue_001 = fits.open('Observing_5_10_2017/Jupiter/p25Seconds/JupiterB_001.fit')
@@ -38,26 +39,34 @@ jupiter_blue_014_image = jupiter_blue_014[0].data
 jupiter_blue_015_image = jupiter_blue_015[0].data
 
 #test to view the numpy array as an image
-#plt.imshow(jupiter_blue_001_image, cmap='Blues')
-#plt.colorbar()
-#plt.show()
+plt.imshow(jupiter_blue_001_image, cmap='Blues')
+plt.colorbar()
+plt.show()
+
+plt.imshow(jupiter_blue_015_image, cmap='Blues')
+plt.colorbar()
+plt.show()
+
+#test to find the dimensions of the image
+#image_size = jupiter_blue_001_image.shape
+#print image_size #1024 x 1360
 
 #stacking numpy arrays into a 3D array
 image_stack_array = []
 image_stack_array.append(jupiter_blue_001_image)
-image_stack_array.append(jupiter_blue_002_image)
-image_stack_array.append(jupiter_blue_003_image)
-image_stack_array.append(jupiter_blue_004_image)
-image_stack_array.append(jupiter_blue_005_image)
-image_stack_array.append(jupiter_blue_006_image)
-image_stack_array.append(jupiter_blue_007_image)
-image_stack_array.append(jupiter_blue_008_image)
-image_stack_array.append(jupiter_blue_009_image)
-image_stack_array.append(jupiter_blue_010_image)
-image_stack_array.append(jupiter_blue_011_image)
-image_stack_array.append(jupiter_blue_012_image)
-image_stack_array.append(jupiter_blue_013_image)
-image_stack_array.append(jupiter_blue_014_image)
+#image_stack_array.append(jupiter_blue_002_image)
+#image_stack_array.append(jupiter_blue_003_image)
+#image_stack_array.append(jupiter_blue_004_image)
+#image_stack_array.append(jupiter_blue_005_image)
+#image_stack_array.append(jupiter_blue_006_image)
+#image_stack_array.append(jupiter_blue_007_image)
+#image_stack_array.append(jupiter_blue_008_image)
+#image_stack_array.append(jupiter_blue_009_image)
+#image_stack_array.append(jupiter_blue_010_image)
+#image_stack_array.append(jupiter_blue_011_image)
+#image_stack_array.append(jupiter_blue_012_image)
+#image_stack_array.append(jupiter_blue_013_image)
+#image_stack_array.append(jupiter_blue_014_image)
 image_stack_array.append(jupiter_blue_015_image)
 
 #median combine 3D array into one numpy array
@@ -67,3 +76,75 @@ final_image = np.median(image_stack_array, axis=0)
 plt.imshow(final_image, cmap='gray')
 plt.colorbar()
 plt.show()
+
+
+##############################################################################################
+
+
+#attempting to allign the two images
+
+def get_gradient(im) :
+    # Calculate the x and y gradients using Sobel operator
+    grad_x = cv2.Sobel(im,cv2.CV_32F,1,0,ksize=3)
+    grad_y = cv2.Sobel(im,cv2.CV_32F,0,1,ksize=3)
+    # Combine the two gradients
+    grad = cv2.addWeighted(np.absolute(grad_x), 0.5, np.absolute(grad_y), 0.5, 0)
+    return grad
+
+
+if __name__ == '__main__':
+    
+    
+    # Read 8-bit color image.
+    # This is an image in which the three channels are
+    # concatenated vertically.
+    
+    im =  cv2.imread("images/emir.jpg", cv2.IMREAD_GRAYSCALE);
+
+    # Find the width and height of the color image
+    sz = im.shape
+    print sz
+    height = int(sz[0] / 3);
+    width = sz[1]
+
+    # Extract the three channels from the gray scale image
+    # and merge the three channels into one color image
+    im_color = np.zeros((height,width,3), dtype=np.uint8 )
+    for i in xrange(0,3) :
+        im_color[:,:,i] = im[ i * height:(i+1) * height,:]
+
+    # Allocate space for aligned image
+    im_aligned = np.zeros((height,width,3), dtype=np.uint8 )
+
+    # The blue and green channels will be aligned to the red channel.
+    # So copy the red channel
+    im_aligned[:,:,2] = im_color[:,:,2]
+
+    # Define motion model
+    warp_mode = cv2.MOTION_HOMOGRAPHY
+
+    # Set the warp matrix to identity.
+    if warp_mode == cv2.MOTION_HOMOGRAPHY :
+            warp_matrix = np.eye(3, 3, dtype=np.float32)
+    else :
+            warp_matrix = np.eye(2, 3, dtype=np.float32)
+
+    # Set the stopping criteria for the algorithm.
+    criteria = (cv2.TERM_CRITERIA_EPS | cv2.TERM_CRITERIA_COUNT, 5000,  1e-10)
+
+    # Warp the blue and green channels to the red channel
+    for i in xrange(0,2) :
+        (cc, warp_matrix) = cv2.findTransformECC (get_gradient(im_color[:,:,2]), get_gradient(im_color[:,:,i]),warp_matrix, warp_mode, criteria)
+    
+        if warp_mode == cv2.MOTION_HOMOGRAPHY :
+            # Use Perspective warp when the transformation is a Homography
+            im_aligned[:,:,i] = cv2.warpPerspective (im_color[:,:,i], warp_matrix, (width,height), flags=cv2.INTER_LINEAR + cv2.WARP_INVERSE_MAP)
+        else :
+            # Use Affine warp when the transformation is not a Homography
+            im_aligned[:,:,i] = cv2.warpAffine(im_color[:,:,i], warp_matrix, (width, height), flags=cv2.INTER_LINEAR + cv2.WARP_INVERSE_MAP);
+        print warp_matrix
+
+    # Show final output
+    cv2.imshow("Color Image", im_color)
+    cv2.imshow("Aligned Image", im_aligned)
+    cv2.waitKey(0)
